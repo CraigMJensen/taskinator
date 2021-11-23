3b7dd3d7c93eb027a75e442b1f42bf5343db0ad7
CraigJensen <cmjensen82@gmail.com>
2021-11-22 18:00:05 -0600

    can now move tasks between to do, in progress, and complete.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector('#task-form');
 var taskToDoEl = document.querySelector('#tasks-to-do');
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector('#page-content');
+var tasksInProgressEl = document.querySelector('#tasks-in-progress');
+var tasksCompletedEl = document.querySelector('#tasks-completed');
 
 var taskFormHandler = function (event) {
 
@@ -18,13 +20,32 @@ var taskFormHandler = function (event) {
 
     formEl.reset();
 
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-    };
+    var isEdit = formEl.hasAttribute('data-task-id');
 
-    createTaskEl(taskDataObj);
+    if (isEdit) {
+        var taskId = formEl.getAttribute('data-task-id');
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
 
+        createTaskEl(taskDataObj);
+    }
+};
+
+var completeEditTask = function (taskName, taskType, taskId) {
+    var taskSelected = document.querySelector('.task-item[data-task-id="' + taskId + '"]');
+
+    taskSelected.querySelector('h3.task-name').textContent = taskName;
+    taskSelected.querySelector('span.task-type').textContent = taskType;
+
+    alert('Task Updated!');
+
+    formEl.removeAttribute('data-task-id');
+    document.querySelector('#save-task').textContent = 'Add Task';
 };
 
 var createTaskEl = function (taskDataObj) {
@@ -113,7 +134,7 @@ var editTask = function (taskId) {
     document.querySelector('select[name="task-type"]').value = taskType;
     document.querySelector('#save-task').textContent = 'Save Task';
     formEl.setAttribute('data-task-id', taskId);
-    console.log(#task - form);
+    console.log();
 };
 
 var deleteTask = function (taskId) {
@@ -121,4 +142,23 @@ var deleteTask = function (taskId) {
     taskSelected.remove();
 };
 
+var taskStatusChangeHandler = function (event) {
+    var taskId = event.target.getAttribute('data-task-id');
+
+    var statusValue = event.target.value.toLowerCase();
+
+    var taskSelected = document.querySelector('.task-item[data-task-id="' + taskId + '"]');
+
+    if (statusValue === 'to do') {
+        taskToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === 'in progress') {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === 'completed') {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 pageContentEl.addEventListener('click', taskButtonHandler);
+pageContentEl.addEventListener('change', taskStatusChangeHandler);
